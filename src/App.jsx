fcd1ada74060bd515802ecbe774bd9c8a115324e
revathi <arevathi125@gmail.com>
2024-03-17 22:13:20 -0500

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import satData from "./components/satData";
 
 function App() {
   const [sat, setSat] = useState(satData);
-  const displaySats = [...new Set(satData.map((data) => {
-    data.orbitType
-  }))]
+  
+  const displaySats = [...new Set(satData.map((data) => data.orbitType ))];
+
   const filterByType = (currentType) => {
     const displaySats = satData.filter(newSatDisplay => {
       return newSatDisplay.orbitType === currentType;
-    })
+    });
     setSat(displaySats);
   };
 
